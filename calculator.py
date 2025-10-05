4014831d6aef52ad03e9a9858f906197397d4fbb
kunalsz <kunalavengers@gmail.com>
2025-10-05 07:59:37 +0530

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# A simple code snippet
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+    
